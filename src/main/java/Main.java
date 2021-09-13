37fb9f7750801ef923c4daa6e53acbfc65640ca6
Mihalea Vlad <vlad1111p@gmail.com>
2021-09-13 17:42:07 +0200

    commit6

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,6 +7,7 @@ import org.hibernate.Session;
 import services.*;
 import view.DoctorLogic;
 import view.LoginLogic;
+import view.MainLogic;
 import view.PatientLogic;
 
 import java.time.LocalDate;
@@ -24,88 +25,8 @@ public class Main {
 
 
         try {
-            DoctorLogic doctorLogic= new DoctorLogic();
-            LoginLogic loginLogic = new LoginLogic();
-            PatientLogic patientLogic = new PatientLogic();
-            Session session = HibernateUtil.getSessionFactory().openSession();
-//            MedicalClinic medicalClinic = new MedicalClinic("MEDSTAR Policlinica Mănăștur",
-//                    "Str. Mehedinţi, nr. 1-3", "0264/917", "email@email.com", new HashSet<Patient>(), new HashSet<Doctor>());
-//            MedicalClinicService medicalClinics = new MedicalClinicService();
-//            medicalClinics.add(medicalClinic);
-//            Doctor doctor = new Doctor("goldman", "felton", "vlad", "vlad",
-//                    new HashSet<Appointment>(), Specialization.DERMATOLOGIST, medicalClinics.findById(1L));
-//            DoctorService doctorService = new DoctorService();
-//            doctorService.add(doctor);
-
-
-            RecipeService recipe = new RecipeService();
-
-            Scanner sc = new Scanner(System.in);
-
-            while (true) {
-                System.out.println("Insert option");
-
-                loginLogic.mainScreen();
-
-                String answer = sc.nextLine();
-
-                if (answer.equals("quit")) {
-
-                    break;
-                } else if (answer.equals("rp")) {
-
-                    patientLogic.registerPatient(sc);
-
-                } else if (answer.equals("lp")) {
-                    while (true) {
-
-                        Patient login = patientLogic.inputLoginPatient(sc);
-
-                        while (login != null) {
-                            patientLogic.loggedPatientOptions(sc);
-                            answer = sc.nextLine();
-
-
-                            if (answer.equals("mp")) {
-
-                                patientLogic.makeAppointment(sc, login);
-
-
-                            } else if (answer.equals("vr")) {
-
-
-                                patientLogic.viewPatientRecipes(login);
-
-
-                            } else if (answer.equals("va")) {
-
-                                patientLogic.viewPatientAppointment(login);
-
-
-                            } else if (answer.equals("quit")) {
-                                break;
-                            }
-
-
-                        }
-
-                    }
-
-
-                } else if (answer.equals("ld")) {
-                    while (true) {
-                        boolean login = doctorLogic.inputLoginDoctor(sc);
-                        if (login) {
-                            System.out.println("youtube");
-
-                        }
-
-                    }
-
-                }
-
-
-            }
+            MainLogic mainLogic = new MainLogic();
+            mainLogic.init();
 
 
         } catch (Exception e) {
